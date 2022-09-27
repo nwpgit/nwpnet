41dfdf84fbd7fdace96fe52b6162ed13c270ce44
nwpgit <114334944+nwpgit@users.noreply.github.com>
2022-09-27 20:44:45 +0900

    nwpnet-feil-1.0.5
    Signed-off-by: nwpgit <114334944+nwpgit@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt install code -y
 sudo apt install gimp -y
+sudo apt-get instal scratch3 -y
 sudo apt-get install libreoffice -y
 sudo apt-get install fcitx-mozc -y
 im-config -n fcitx
